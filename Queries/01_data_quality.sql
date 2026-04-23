e60a428448589f0bf5596c807f30a3c8bf1c2a37
kesh99 <keshikawije0817@gmail.com>
2026-04-23 11:37:03 +0530

    Create project structure

---
+++ Queries/01_data_quality.sql
@@ -0,0 +1,27 @@
+-- How many rows exist in each table?
+
+-- Are there duplicate primary keys in any table?
+
+-- Are there duplicate customer emails?
+
+-- Are there duplicate staff emails?
+
+-- Which tables contain null values in important columns?
+
+-- Are any products missing prices?
+
+-- Are any orders missing dates?
+
+-- Are there orders without customers?
+
+-- Are there order items without orders?
+
+-- Are there order items without products?
+
+-- Are there stocks linked to missing stores?
+
+-- Are discounts outside valid range (0–1)?
+
+-- Are there negative quantities or prices?
+
+-- Which tables need cleaning before analysis?
