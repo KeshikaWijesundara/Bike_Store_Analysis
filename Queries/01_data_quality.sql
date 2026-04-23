845bc320298e5fd4b71e8bdc837757697a6358dc
kesh99 <keshikawije0817@gmail.com>
2026-04-23 12:49:29 +0530

    duplicate primary keys?

--- Queries/01_data_quality.sql
+++ Queries/01_data_quality.sql
@@ -19,6 +19,60 @@ SELECT 'stores' AS tale_name,COUNT(*) AS row_count FROM stores
 
 
 -- Are there duplicate primary keys in any table?
+--Brands Table
+SELECT brand_id,count(*)
+FROM brands
+GROUP BY brand_id
+HAVING count(brand_id) >1
+
+--Categories Table
+SELECT category_id,COUNT(*)
+FROM categories
+GROUP BY category_id
+HAVING COUNT(category_id) >1
+
+--Customers Table
+SELECT customer_id,count(*)
+FROM customers
+GROUP BY customer_id
+HAVING COUNT(customer_id) > 1
+
+--Order_items Table
+SELECT order_id,item_id,count(*)
+FROM order_items
+GROUP BY order_id,item_id
+HAVING COUNT(*) > 1
+
+--Orders Table
+SELECT order_id,count(*)
+FROM orders
+GROUP BY order_id
+HAVING COUNT(order_id) > 1
+
+--Products Table
+SELECT product_id,count(*)
+FROM products
+GROUP BY product_id
+HAVING COUNT(product_id) > 1
+
+--Staffs Table
+SELECT staff_id,count(*)
+FROM staffs
+GROUP BY staff_id
+HAVING COUNT(staff_id) > 1
+
+--Stocks Table
+SELECT store_id,product_id,count(*)
+FROM stocks
+GROUP BY store_id,product_id
+HAVING COUNT(*) > 1
+
+--Stores Table
+SELECT store_id,count(*)
+FROM stores
+GROUP BY store_id
+HAVING COUNT(store_id) > 1
+
 
 -- Are there duplicate customer emails?
 
